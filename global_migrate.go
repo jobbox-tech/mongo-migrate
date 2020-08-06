88a21015868cf794371ef623238c55c532a97ec3
agelloz <antoine.gelloz@gmail.com>
2020-08-06 14:10:37 +0200

    module path

--- global_migrate.go
+++ global_migrate.go
@@ -40,7 +40,7 @@ func internalRegister(up, down MigrationFunc, skip int) error {
 // 	 "go.mongodb.org/mongo-driver/bson"
 // 	 "go.mongodb.org/mongo-driver/mongo"
 // 	 "go.mongodb.org/mongo-driver/mongo/options"
-// 	 "github.com/xakep666/mongo-migrate"
+// 	 "github.com/metronlab/mongo-migrate"
 //  )
 //
 //  func init() {
