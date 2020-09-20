44cf76f65a28e8d4cf7a41406bf1aeb99ccb18f9
Gufran Mirza <gufranmirza1@gmail.com>
2020-09-20 10:49:58 +0530

    fix module path

--- global_migrate.go
+++ global_migrate.go
@@ -40,7 +40,7 @@ func internalRegister(up, down MigrationFunc, skip int) error {
 // 	 "go.mongodb.org/mongo-driver/bson"
 // 	 "go.mongodb.org/mongo-driver/mongo"
 // 	 "go.mongodb.org/mongo-driver/mongo/options"
-// 	 "github.com/metronlab/mongo-migrate"
+// 	 "github.com/jobbox-tech/mongomigrate"
 //  )
 //
 //  func init() {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/metronlab/mongo-migrate
+module github.com/jobbox-tech/mongomigrate
 
 require (
 	github.com/go-stack/stack v1.8.0 // indirect
